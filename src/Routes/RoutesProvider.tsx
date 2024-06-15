7d133f18a3689eb6709a4e67ffa597fde7c675c4
Dian <dian7875.a@gmail.com>
2024-06-15 14:11:24 -0600

    First Comit

---
+++ src/Routes/RoutesProvider.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom"
+
+const RoutesProvider = createBrowserRouter(
+
+    [
+        {
+          path: "/",
+          element: < />
+        },
+        {
+          path: "/Login",
+          element: < />
+        }
+      ]
+)
+
+export default RoutesProvider
